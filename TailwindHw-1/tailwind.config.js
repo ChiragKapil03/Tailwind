10992be2961f9dbca0ad5fd9f798ad2c88e30f3d
Chirag Kapil <your.email@example.com>
2024-06-20 19:50:44 +0530

    comitting hW1

---
+++ TailwindHw-1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./hw1.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Inconsolata': ['Inconsolata', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
